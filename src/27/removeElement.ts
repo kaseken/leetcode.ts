2576e5ab8d07390cf3393ff35e884ceda47f0c66
kaseken <k.kaseda0925@gmail.com>
2025-02-16 16:59:21 +0900

    feat: Add src/27/removeElement.ts

---
+++ src/27/removeElement.ts
@@ -0,0 +1,14 @@
+export const removeElement = (nums: number[], target: number): number => {
+  let rIndex = nums.length
+  for (let lIndex = 0; lIndex < nums.length && lIndex < rIndex; lIndex++) {
+    if (nums[lIndex] !== target) {
+      continue
+    }
+    rIndex--
+    while (lIndex < rIndex && nums[rIndex] === target) {
+      rIndex--
+    }
+    nums[lIndex] = nums[rIndex]
+  }
+  return rIndex
+}
