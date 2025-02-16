4b5572f917615eda07b86dd9c48db3941b2c0787
Kent Kaseda <kent.kaseda.dev@gmail.com>
2025-02-16 15:45:26 +0900

    feat: Add src/71/simplifyPath.ts (#1)

--- src/71/simplifyPath.test.ts
+++ src/71/simplifyPath.test.ts
@@ -1,5 +1,9 @@
 import { simplifyPath } from './simplifyPath'
 
 test('Test simplifyPath', () => {
-  expect(simplifyPath('')).toBe('')
+  expect(simplifyPath('/home/')).toBe('/home')
+  expect(simplifyPath('/home//foo/')).toBe('/home/foo')
+  expect(simplifyPath('/home/user/Documents/../Pictures')).toBe('/home/user/Pictures')
+  expect(simplifyPath('/../')).toBe('/')
+  expect(simplifyPath('/.../a/../b/c/../d/./')).toBe('/.../b/d')
 })
--- src/71/simplifyPath.ts
+++ src/71/simplifyPath.ts
@@ -1,3 +1,21 @@
+type Dir = 'DirOrFileName' | 'CurrentDir' | 'ParentDir' | 'None'
+
+const parseDir = (dir: string): Dir => {
+  if (dir === '') return 'None'
+  if (dir === '.') return 'CurrentDir'
+  if (dir === '..') return 'ParentDir'
+  return 'DirOrFileName'
+}
+
 export const simplifyPath = (path: string): string => {
-  return path
+  const stack: string[] = []
+  path.split('/').forEach((dir) => {
+    const dirType = parseDir(dir)
+    if (dirType === 'ParentDir') {
+      stack.pop()
+    } else if (dirType === 'DirOrFileName') {
+      stack.push(dir)
+    }
+  })
+  return '/' + stack.join('/')
 }
