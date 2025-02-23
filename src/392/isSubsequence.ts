4d1663aa16e7f19d42c326f7463da3940cb02ffc
kaseken <k.kaseda0925@gmail.com>
2025-02-23 23:54:19 +0900

    feat: Add rotate.ts

--- src/392/isSubsequence.ts
+++ src/392/isSubsequence.ts
@@ -1,11 +1,11 @@
 export const isSubsequence = (s: string, t: string): boolean => {
-  let sIndex = 0
-  let tIndex = 0
+  let sIndex = 0;
+  let tIndex = 0;
   while (sIndex < s.length && tIndex < t.length) {
     if (s[sIndex] === t[tIndex]) {
-      sIndex++
+      sIndex++;
     }
-    tIndex++
+    tIndex++;
   }
-  return sIndex === s.length
-}
+  return sIndex === s.length;
+};
